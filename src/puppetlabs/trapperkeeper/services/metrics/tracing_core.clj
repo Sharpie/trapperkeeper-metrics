31b56d44a0c2de779cf9eb63d3dc1dc3b4032330
Charlie Sharpsteen <chuck@puppet.com>
2017-01-01 13:09:59 -0800

    (PE-13491) (SPIKE) Add ring handlers
    This patch adds a ring handler which wraps incoming requests in an
    OpenTracing span.

--- src/puppetlabs/trapperkeeper/services/metrics/tracing_core.clj
+++ src/puppetlabs/trapperkeeper/services/metrics/tracing_core.clj
@@ -1,14 +1,18 @@
 (ns puppetlabs.trapperkeeper.services.metrics.tracing-core
   "Clojure helpers for constructing and configuring Tracing."
   (:require [clojure.tools.logging :as log]
-            [schema.core :as schema])
+            [schema.core :as schema]
+            [clojure.set :as setutils]
+            [ring.util.request :as requtils])
   (:import
+    [io.opentracing.propagation Format$Builtin TextMapExtractAdapter]
+    [io.opentracing.tag Tags]
+
     [com.uber.jaeger Tracer$Builder]
     [com.uber.jaeger.context TracingUtils]
     [com.uber.jaeger.samplers ConstSampler]
     [com.uber.jaeger.reporters LoggingReporter RemoteReporter]
     [com.uber.jaeger.metrics InMemoryStatsReporter Metrics StatsFactoryImpl]
-    [io.opentracing.propagation Format$Builtin]
     [com.uber.jaeger.propagation.b3 B3TextMapCodec]
     [com.uber.jaeger.senders.zipkin ZipkinSender]))
 
@@ -46,3 +50,28 @@
 (defn get-current-span
   []
   (.getCurrentSpan trace-context))
+
+
+(defn extract-span-from-req
+  [tracer req]
+  (-> tracer
+       (.buildSpan (requtils/path-info req))
+       (.asChildOf (.extract tracer
+                             Format$Builtin/TEXT_MAP
+                             (TextMapExtractAdapter. (:headers req))))
+       (.withTag (.getKey Tags/SPAN_KIND) Tags/SPAN_KIND_SERVER)
+       .start))
+
+(defn wrap-with-request-tracing
+  "Ring middleware. Wraps the given ring handler with OpenTracing instrumentation."
+  [app tracer]
+  (fn [req]
+    (let [span (extract-span-from-req tracer req)
+          traced-req (assoc req :opentracing-span span)]
+      (try
+        (push-span span)
+        (app traced-req)
+        (finally
+          (.finish span)
+          (pop-span))))))
+
