f689e5be5476461ca4a2a5f4d98018f455495a71
Charlie Sharpsteen <chuck@puppet.com>
2020-08-27 18:02:40 -0700

    (maint) Update to latest 1.x clj-parent
    This commit updates project.clj to use clj-parent 1.7.38 which is the
    latest used by the Puppet 5.x release series.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :min-lein-version "2.7.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "1.5.1"]
+  :parent-project {:coords [puppetlabs/clj-parent "1.7.38"]
                    :inherit [:managed-dependencies]}
 
   :pedantic? :abort
